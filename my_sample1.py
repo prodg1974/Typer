6a8e568d29cd39b5d079ac97b8480c6361192570
Paul Rodgers <paul_rodg@yahoo.com>
2020-01-18 18:11:12 -0500

    Running with Postgres (docker). Cannot get dotenv to work properly

--- my_sample1.py
+++ my_sample1.py
@@ -1,3 +1,22 @@
+import os
+import logging
+from pathlib import Path
+
 import typer
+from dotenv import load_dotenv, find_dotenv
+import sqlalchemy as sa
+
+logging.basicConfig(level=logging.DEBUG)
+
+home = Path.home()
+env_path = Path(home,'.env')
+logging.debug(f'dotenv_path: {env_path}')
+
+load_dotenv(env_path,verbose=True)
+pg_password = os.environ.get('POSTGRES_PASSWORD')
+logging.debug(f'pg_password: {pg_password}')
+engine = sa.create_engine(f'postgresql://postgres:{pg_password}@localhost:5432/postgres')
+engine.execute("select 1")
 
 def hello(name:str):
+    pass
